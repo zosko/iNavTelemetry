d77615f397e61ce4eef42ac408c9c9875726c6e3
bosko <bosko.petreski@gmail.com>
2020-05-28 16:18:33 +0200

    Initial Commit

---
+++ iNavTelemetry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iNavTelemetry
+//
+//  Created by Bosko Petreski on 5/28/20.
+//  Copyright © 2020 Bosko Petreski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
