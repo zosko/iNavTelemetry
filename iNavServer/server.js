1a752b3c81856f9fff02daa3f46dadd4105d2e58
Bosko <bosko.petreski@gmail.com>
2020-10-25 21:46:56 +0100

    Update server.js

--- iNavServer/server.js
+++ iNavServer/server.js
@@ -11,13 +11,17 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function(req, res){
   res.sendFile('public/index.html', { root : __dirname })
 })
+
 app.get('/restart', function(req, res){
   database = {};
   res.sendFile('public/index.html', { root : __dirname })
 })
 
 io.sockets.on("connection", function(socket) {
-  setInterval(function(){ socket.emit("planesLocation", database); }, 1500);
+  setInterval(function(){ 
+    var planes = Object.keys(database).map(key => database[key]);
+    socket.emit("planesLocation", planes); 
+  }, 2000);
   
   //send to everyone -> io.sockets.emit('planes',plane);
   //send to everyone except for sender -> socket.broadcast.emit('planes',plane); 
@@ -27,7 +31,7 @@ io.sockets.on("connection", function(socket) {
   // socket.roomnum
   
   socket.on("planeLocation", function(plane) {
-    database[socket.id] = {lat:plane.lat,lng:plane.lng,alt:plane.alt,speed:plane.speed,heading:plane.heading};
+    database[socket.id] = {lat:plane.lat,lng:plane.lng,alt:plane.alt,speed:plane.speed,heading:plane.heading,photo:plane.photo};
   });
   socket.on("disconnect", function() {
     //remove plane from database
