8ee8542dbbe5e20f536515d34bf37748c134cfe5
bosko <bosko.petreski@gmail.com>
2021-09-05 12:02:38 +0200

    Initial Commit

---
+++ iNavTelemetry/iNavTelemetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNavTelemetryApp.swift
+//  iNavTelemetry
+//
+//  Created by Bosko Petreski on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct iNavTelemetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
